acc23396c234c7e1b81fce44ea9010b1d610b835
giorgi <giorgikochuashvili2002@gmail.com>
2023-01-29 01:03:30 +0400

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [list, setList] = useState([
+    {
+      id: Math.random(),
+      completed: false,
+      todo: 'CLEAR ME',
+    },
+  ]);
+
+  const addItem = (todo) => {
+    if (!input) return;
+    // create todo with id
+    const newTodo = {
+      id: Math.random(),
+      completed: false,
+      todo,
+    };
+    // add it to the list but do not overwrite on old values
+    setList([...list, newTodo]);
+    // clear field
+    setInput('');
+    console.log(list);
+  };
+  const completeTask = (id) => {
+    const findTodo = list.find((todo) => todo.id === id);
+    findTodo.completed = !findTodo.completed;
+    console.log(findTodo);
+  };
+  const removeItem = (id) => {
+    const newtodos = list.filter((todo) => todo.id !== id);
+    setList(newtodos);
+  };
+  const clearCompleted = () => {
+    const newtodos = list.filter((todo) => todo.completed === false);
+    setList(newtodos);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=''>
+      <main>
+        <input
+          type='text'
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button onClick={() => addItem(input)}>add</button>
+      </main>
+      {list.map((todo) => {
+        return (
+          <div className='item' key={todo.id}>
+            <input type='checkbox' onClick={() => completeTask(todo.id)} />
+            <p>{todo.todo}</p>
+            <button onClick={() => removeItem(todo.id)}>&times;</button>
+          </div>
+        );
+      })}
+      <button onClick={clearCompleted}>clear copleted</button>
     </div>
   );
 }
